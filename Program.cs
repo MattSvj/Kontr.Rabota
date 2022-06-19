0fa3019f9853eaad6a76de9dfcb82ea699763bb1
Matthew <matthew-03@mail.ru>
2022-06-19 14:25:06 +0300

    Спустя миллион лет оно работает!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿//Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
 
 string[] names = {"hello", "2", "world", ":-)"};
+
+void CountEven(string[] words)
+{   
+    string[] a = new string[4]; 
+    for(int i = 0; i < words.Length; i++)
+    {
+        if(words[i].Length <= 3) a[i] += words[i];
+    }
+    a = a.Where(x => x != null).ToArray();
+    for(int j = 0; j < a.Length; j++)
+                Console.Write(a[j] + " ");
+}
+
+CountEven(names);
